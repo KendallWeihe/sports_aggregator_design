19d05d8298a9f523c32848f9e30a4e090ad24038
Kendall Weihe <kendallweihe@gmail.com>
2017-10-22 09:56:37 -0400

    collected data fro previous two seasons

--- mysportsfeed/collect_play_by_play.py
+++ mysportsfeed/collect_play_by_play.py
@@ -5,57 +5,114 @@ import time
 
 from ohmysportsfeedspy import MySportsFeeds
 import json
-import datetime
+import sys
 
 msf = MySportsFeeds(version="1.0")
-
 msf.authenticate("kendallweihe", os.environ["PWD_MSF"])
 
-game_dates = []
-for i in range(20161025, 20170701):
-    game_dates.append(str(i))
+def get_dates():
+    game_dates = []
+    year = "2014"
+    for i in range(11, 13):
+        for j in range(1, 32):
+            date = "{}{}{}".format(year, i, str(j).zfill(2))
+            game_dates.append(date)
+    year = "2015"
+    for i in range(1, 8):
+        for j in range(1, 32):
+            date = "{}{}{}".format(year, str(i).zfill(2), str(j).zfill(2))
+            game_dates.append(date)
 
-requests_count = 0
-time_start = datetime.datetime.now()
-pdb.set_trace()
-for date in game_dates:
+    return game_dates
 
+def get_daily_schedule(date):
+    time.sleep(5)
     schedule = msf.msf_get_data(
                                 league="nba",
                                 feed="daily_game_schedule",
-                                season="2016-2017-regular",
+                                season="2014-2015-regular",
                                 format="json",
                                 fordate=date,
-                                force=True
+                                force=True,
+                                store_file=None
                                 )
-    requests_count += 1
+    return schedule
 
-    if (not schedule.get("dailygameschedule")) or (not schedule["dailygameschedule"].get("gameentry")):
-        continue
+def get_play_by_play(game_id):
+    time.sleep(5)
+    play_by_play = msf.msf_get_data(
+                                    league="nba",
+                                    feed="game_playbyplay",
+                                    season="2014-2015-regular",
+                                    format="json",
+                                    gameid=game_id,
+                                    force=True,
+                                    store_file=None
+                                    )
+    return play_by_play
 
-    for game in schedule["dailygameschedule"]["gameentry"]:
-        game_id = "{}-{}-{}".format(date, game["awayTeam"]["Abbreviation"], game["homeTeam"]["Abbreviation"])
-        play_by_play = msf.msf_get_data(
-                                        league="nba",
-                                        feed="game_playbyplay",
-                                        season="2016-2017-regular",
-                                        format="json",
-                                        gameid=game_id,
-                                        force=True
-                                        )
-                                        
-        f = open("/Users/kendallweihe/Development/msf_data/NBA_play_by_play/{}.json".format(game_id), "w")
-        f.write(json.dumps(play_by_play, indent=4))
-        f.close()
+def main():
+    requests_count = 0
+    game_dates = get_dates()
+    for date in game_dates:
 
-        os.system("rm results/*")
+        found_error = False
+        while not found_error:
+            try:
+                schedule = get_daily_schedule(date)
+                break
+            except Exception as e:
+                error = str(e)
+                print(error)
+                if "429" in error:
+                    # sleep for a minute and try again
+                    print("Error getting schedule for {}...\nSleeping for 60 seconds and trying again".format(date))
+                    time.sleep(60)
+                elif "400" in error:
+                    print("400 Error...")
+                    found_error = True
+                else:
+                    pdb.set_trace()
+                    sys.exit()
 
-        requests_count += 1
-        if requests_count > 250:
-            time_end = datetime.datetime.now()
-            seconds_passed = (time_start - time_end).total_seconds()
-            wait_time = (60*5 - seconds_passed)
-            time.sleep(wait_time)
+        if found_error:
+            continue
 
-            requests_count = 0
-            time_start = datetime.datetime.now()
+        if (not schedule.get("dailygameschedule")) or (not schedule["dailygameschedule"].get("gameentry")):
+            print("No games found for {}".format(date))
+            continue
+
+        for game in schedule["dailygameschedule"]["gameentry"]:
+            game_id = "{}-{}-{}".format(date, game["awayTeam"]["Abbreviation"], game["homeTeam"]["Abbreviation"])
+
+            found_error = False
+            while not found_error:
+                try:
+                    play_by_play = get_play_by_play(game_id)
+                    break
+                except Exception as e:
+                    error = str(e)
+                    print(error)
+                    if "429" in error:
+                        # sleep for a minute and try again
+                        print("Error getting game for {}...\nSleeping for 60 seconds and trying again".format(game_id))
+                        time.sleep(60)
+                    elif "400" in error:
+                        print("400 Error...")
+                        found_error = True
+                    else:
+                        pdb.set_trace()
+                        sys.exit()
+
+            if found_error:
+                continue
+
+            f = open("{}/{}.json".format(os.environ["OUT_DIR"], game_id), "w")
+            f.write(json.dumps(play_by_play, indent=4))
+            f.close()
+
+            print("Collected {}".format(game_id))
+            os.system("rm results/*")
+
+if __name__ == "__main__":
+    main()
