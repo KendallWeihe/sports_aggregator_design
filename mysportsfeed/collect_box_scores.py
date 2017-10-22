d0741bc0d85d35b24af9b596b274e791e3f799f4
Kendall Weihe <kendallweihe@gmail.com>
2017-10-22 14:59:53 -0400

    collecting box scores...

--- mysportsfeed/collect_box_scores.py
+++ mysportsfeed/collect_box_scores.py
@@ -30,7 +30,7 @@ def get_daily_schedule(date):
     schedule = msf.msf_get_data(
                                 league="nba",
                                 feed="daily_game_schedule",
-                                season="2014-2015-regular",
+                                season="2016-2017-regular",
                                 format="json",
                                 fordate=date,
                                 force=True,
@@ -38,24 +38,25 @@ def get_daily_schedule(date):
                                 )
     return schedule
 
-def get_play_by_play(game_id):
+def get_box_score(game_id):
     time.sleep(5)
-    play_by_play = msf.msf_get_data(
+    box_score = msf.msf_get_data(
                                     league="nba",
-                                    feed="game_playbyplay",
-                                    season="2014-2015-regular",
+                                    feed="game_boxscore",
+                                    season="2016-2017-regular",
                                     format="json",
                                     gameid=game_id,
                                     force=True,
                                     store_file=None
                                     )
-    return play_by_play
+    return box_score
 
 def main():
     requests_count = 0
     game_dates = get_dates()
     for date in game_dates:
 
+        date = "20161025"
         found_error = False
         while not found_error:
             try:
@@ -69,7 +70,7 @@ def main():
                     print("Error getting schedule for {}...\nSleeping for 60 seconds and trying again".format(date))
                     time.sleep(60)
                 elif "Max retries" in error:
-                    print("Max retries error for {}...\nSleeping for 60 seconds".format(date))
+                    print("Max retries error for {}...\nSleeping for 60 seconds".format(game_id))
                     time.sleep(180)
                 elif "400" in error:
                     print("400 Error...")
@@ -91,7 +92,7 @@ def main():
             found_error = False
             while not found_error:
                 try:
-                    play_by_play = get_play_by_play(game_id)
+                    box_score = get_box_score(game_id)
                     break
                 except Exception as e:
                     error = str(e)
@@ -114,7 +115,7 @@ def main():
                 continue
 
             f = open("{}/{}.json".format(os.environ["OUT_DIR"], game_id), "w")
-            f.write(json.dumps(play_by_play, indent=4))
+            f.write(json.dumps(box_score, indent=4))
             f.close()
 
             print("Collected {}".format(game_id))
