e626e10fb8d920aa59f3bdb8993f05a344c27181
Kendall Weihe <kendallweihe@gmail.com>
2017-11-03 17:40:18 -0400

    fair amount of work done in the data generator logic -- still need to enumerate stat values & save to ouput files

--- data_generator/generate_play_by_play.go
+++ data_generator/generate_play_by_play.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "fmt"
+    "io/ioutil"
     // "reflect"
 )
 
@@ -14,49 +15,99 @@ type DataFile struct {
   rows []Row
 }
 
-func get_data_keys(data_types *JSONList) []string {
-  var data_keys []string
-  for key, json_list := range data_types.json_list {
-    fmt.Printf("key[%s]\nvalues[%s]\n", key, json_list)
-    num_attributes := len(json_list.values)
+type StatType struct {
+  name string
+  keys []string
+}
 
-    var json_keys []string
+// func find_stat_type(stat_type string) StatType {
+//
+// }
+
+func get_stat_type_keys(name string, stat_types []StatType) []string {
+  for _, stat_type := range stat_types {
+    if stat_type.name == name {
+      return stat_type.keys
+    }
+  }
+  var empty_str []string
+  return empty_str
+}
+
+func get_stat_types(f_stat_types *JSON) ([]StatType, int) {
+  var stat_types []StatType
+  num_columns := 0
+
+  for key, json_list := range f_stat_types.json_list {
+    fmt.Printf("key[%s]\nvalues[%s]\n", key, json_list)
+
+    var stat_type StatType
+    stat_type.name = key
+
+    num_attributes := len(json_list.values)
     index := 0
     for index < num_attributes {
       json_key := key + "." + json_list.values[index]
-      fmt.Printf("%s\n", json_key)
-      json_keys = append(json_keys, json_key)
+      stat_type.keys = append(stat_type.keys, json_key)
       index += 1
     }
 
-    data_keys = append(data_keys, json_keys)
-    fmt.Printf("\n")
+    stat_types = append(stat_types, stat_type)
+
+    if num_attributes > num_columns {
+      num_columns = num_attributes
+    }
   }
-  return data_keys
+
+  return stat_types, num_columns
 }
 
 func main() {
 
+  // READ CONFIG FILE ------------------------------------
   config_file := "config.json"
   config := new(JSON)
   read_json(config_file, config)
 
   input_path, _, _ := find(config, "input_path")
-  output_path, _, _ := find(config, "output_path")
-  data_list_key, _, _ := find(config, "data_list_key")
-  _, data_types, _ := find(config, "data_types")
-  fmt.Printf("Input path: %s\n", input_path)
-  fmt.Printf("Output path: %s\n", output_path)
-  fmt.Printf("Data list key: %s\n", data_list_key)
-  fmt.Printf("Data types: %v\n\n", data_types)
-  fmt.Printf("data_types.key_value: %v\n", data_types.key_value)
-  fmt.Printf("data_types.json_nested: %v\n", data_types.json_nested)
-  fmt.Printf("data_types.json_list: %v\n\n", data_types.json_list)
+  // output_path, _, _ := find(config, "output_path")
+  plays_key, _, _ := find(config, "plays_key")
+  _, f_stat_types, _ := find(config, "stat_types")
 
-  data_keys := get_data_keys(data_types)
-  fmt.Printf("data_keys: %s", data_keys)
+  stat_types, num_columns := get_stat_types(f_stat_types)
+  fmt.Printf("\nstat_types: %s\n", stat_types)
 
-  // TODO:
-  //   - define DataFile with data_keys
+  // READ DATA FILES ------------------------------------
+  input_files, err := ioutil.ReadDir(*input_path)
+  check(err)
+  for _, file := range input_files {
+    file_path := *input_path + "/" + file.Name()
+    input_data := new(JSON)
+    read_json(file_path, input_data)
+
+    _, _, plays := find(input_data, *plays_key)
+    for _, play := range plays.json_objs {
+      quarter := play.key_value["quarter"]
+      time := play.key_value["time"]
+      fmt.Printf("Quarter: [%s] Time [%s]\n", quarter, time)
+
+      index := 0
+      row := make([]string, num_columns)
+      row[index] = quarter
+      row[index] = time
+
+      for name, _ := range play.json_nested {
+        stat_type_keys := get_stat_type_keys(name, stat_types)
+        for _, stat_type_key := range stat_type_keys {
+          play_value, _, _ := find(play, stat_type_key)
+          // TODO: enumerate the play_value
+          row[index] = *play_value
+          index += 1
+        }
+      }
+
+      fmt.Printf("Row: [%v]\n", row)
+    }
+  }
 
 }
